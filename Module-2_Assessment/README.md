e7b8e1f9cff1f4c8048ddb6b96a3c212c300487b
drainganggtb <jskoylesgreenberg@gmail.com>
2020-06-16 21:03:31 -0500

    final hangman

---
+++ Module-2_Assessment/README.md
@@ -0,0 +1,4 @@
+This is a Hangman game I created which draws inspiration from the video game Animal Crossing.
+All of the words relate to characters in the game. 
+This project tested my application of booleans, strings, numbers, loops, conditionals, functions, objects, and event listeners. 
+To play, guess 9 times in order to elucidate the name of the character. Have fun! 
--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,88 +1,86 @@
+var acCharacters = ["BLATHERS","TOMNOOK","GULLIVER","FLICK","REDD","LEIF","ISABELLE","CELESTE","DAISYMAE"];
+var totalGuesses = 9;       // number of tries
+var userGuesses = [];       // letters the user guessed
+var computerPick;           // computer will choose which index is chosen
+var wordGuessed = [];       // formed during guessing
+var guessesLeft = 0;        // guesses left. self explanatory
+var finishedGame = false;   // press any key to try again     
+var wins = 0;               //wins
+var losses = 0;             //losses
+var key = new Audio('sounds/key.mp3');
 
-var acwords = ["BLATHERS","TOMNOOK","GULLIVER","FLICK","REDD","LEIF","ISABELLE","CELESTE","DAISYMAE"];
-var totalGuesses = 8; //number of tries
-var userGuesses = []   //letters guessed by user
-var wordpick;          //what word will be chosen by computer
-var wordGuessed = []   //word that is being built
-var guessesLeft = 0;   //how many more attempts the user gets
-var finishedGame = false; //will allow for restart
-var wins = 0;           //wins
-var losses = 0;         //losses
-
-//audio keypress, win, loss
-var keypress = new Audio('./assets/sounds/key.mp3');
-var winsound = new Audio('./assets/sounds/win.mp3');
-var losesound = new Audio('./assets/sounds/loss.mp3');
-
-//start the game
-function startGame () {
+// start the game
+function startGame() {
     guessesLeft = totalGuesses;
 
-    //choose random index which will determine word
-    wordpick = Math.floor(Math.random() * (acwords.length));
+    //grab a random number from the acCharacters array
+    computerPick = Math.floor(Math.random() * (acCharacters.length));
 
-    //will place picture based on what word is chosen
+    if(acCharacters[computerPick] == acCharacters[0]) {
+        $('.clue').html("<img src='assets/images/blathers.png' width='300'/>");
+    }else if(acCharacters[computerPick] == acCharacters[1]) {
+        $('.clue').html("<img src='assets/images/tomnook.png' width='300'/>");
+    }else if(acCharacters[computerPick] == acCharacters[2]) {
+        $('.clue').html("<img src='assets/images/gulliver.png' width='300'/>");
+    }else if(acCharacters[computerPick] == acCharacters[3]) {
+        $('.clue').html("<img src='assets/images/flick.png' width='300'/>");
+    }else if(acCharacters[computerPick] == acCharacters[4]) {
+        $('.clue').html("<img src='assets/images/redd.png' width='300'/>");  
+    }else if(acCharacters[computerPick] == acCharacters[5]) {
+        $('.clue').html("<img src='assets/images/leif.png' width='300'/>");  
+    }else if(acCharacters[computerPick] == acCharacters[6]) {
+        $('.clue').html("<img src='assets/images/isabelle.png' width='300'/>");  
+    }else if(acCharacters[computerPick] == acCharacters[7]) {
+        $('.clue').html("<img src='assets/images/celeste.png' width='300'/>");  
+    }else if(acCharacters[computerPick] == acCharacters[8]) {
+        $('.clue').html("<img src='assets/images/daisymae.png' width='300'/>");                                     
+    }else($('.clue').text('neither of these')); 
 
-    if (acwords[wordpick] == acwords[0])
-        $('.acpic').html("<img src='assets/images/blathers.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[1]) {
-        $('.acpic').html("<img src='assets/images/tomnook.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[2]) {
-        $('.acpic').html("<img src='assets/images/gulliver.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[3]) {
-        $('.acpic').html("<img src='assets/images/fl{ck.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[4]) {
-        $('.acpic').html("<img src='assets/images/redd.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[5]) {
-        $('.acpic').html("<img src='assets/images/leif.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[6]) {
-        $('.acpic').html("<img src='assets/images/isabelle.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[7]) {
-        $('.acpic').html("<img src='assets/images/celeste.png' width='250'/>");
-    }else if(acwords[wordpick] == acwords[8]) {
-        $('.acpic').html("<img src='assets/images/daisymae.png' width='250'/>");
-    }else($('.acpic').text('none'));
+    // Clear out arrays
+    userGuesses = [];
+    wordGuessed = [];
 
-    userGuesses =[];
-    wordGuessed =[];
-
-    //start guessing word with blanks
-    for (var i = 0; i < acwords[wordpick].length, i++) {
+    //start guessing the word using blanks
+    for (var i = 0; i < acCharacters[computerPick].length; i++) {
         wordGuessed.push("_");
-    }
+    }   
 
-    //win, lose, try again
+    //gamewin, gameover, title 
     document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
-    document.getElementById("gameover").style.cssText = "display: none";
-    document.getElementById("youwin").style.cssText = "display: none";
+    document.getElementById("gameover-image").style.cssText = "display: none";
+    document.getElementById("youwin-image").style.cssText = "display: none";
 
-    refreshscreen();
+    //refresh the screen
+    refreshScreen();
 };
 
-//update display on HTML
-function refreshscreen() {
-    document.getElementById("gamewins").innertext = wins;
-    document.getElementById("gamelosses").innertext = losses;
+//  Updates the display on the HTML Page
+function refreshScreen() {
 
-    var guessing = "";
-    for (var i=0;i<wordGuessed.length; i++) {
-        guessing += wordGuessed[i];
+    document.getElementById("gameWins").innerText = wins;
+    document.getElementById("gameLosses").innerText = losses;
+
+    var guessingWordText = "";
+    for (var i = 0; i < wordGuessed.length; i++) {
+        guessingWordText += wordGuessed[i];
     }
 
-    //update what was entered - guesses, word, letters
-    document.getElementById("currentword").innertext = guessing;
-    document.getElementById("guessesLeft").innertext = guessesLeft;
-    document.getElementById("userGuesses").innertext = userGuesses;
+    //update guesses, word, and letters entered
+    document.getElementById("currentWord").innerText = guessingWordText;
+    document.getElementById("guessesLeft").innerText = guessesLeft;
+    document.getElementById("userGuesses").innerText = userGuesses;
 };
-//compare letter entered and word
-function evaluateguess(letter) {
+
+//compare letters entered to the character you're trying to guess
+function evaluateGuess(letter) {
     var positions = [];
 
-    for (var i = 0; i < acnames[wordpick].length; i++) {
-        if (acwords[wordpick][i] === letter) {
+    for (var i = 0; i < acCharacters[computerPick].length; i++) {
+        if(acCharacters[computerPick][i] === letter) {
             positions.push(i);
         }
     }
+
     if (positions.length <= 0) {
         guessesLeft--;
     } else {
@@ -92,52 +90,53 @@ function evaluateguess(letter) {
     }
 };
 
-//check if word was guessed
-function didyouwin() {
+//check if all letters have been entered.
+function checkWin() {
     if(wordGuessed.indexOf("_") === -1) {
-        document.getElementById("youwin").style.csstext = "display: block";
-        document.getElementById("presskeytryagain").style.csstext = "display: block";
-        winsound.play()
+        document.getElementById("youwin-image").style.cssText = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText= "display: block";
         wins++;
         finishedGame = true;
     }
 };
 
-//run out of guesses
-function didyoulose() {
+//check if the user is out of guesses
+function checkLoss()
+{
     if(guessesLeft <= 0) {
-        document.getElementById("gameover").style.csstext = "display:block";
-        document.getElementById("presskeytryagain").style.csstext = "display:block";
-        losesound.play()
+        document.getElementById("gameover-image").style.cssText = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText = "display: block";
         losses++;
         finishedGame = true;
     }
 }
+
 //guessing
 function makeGuess(letter) {
     if (guessesLeft > 0) {
-        if(userGuesses.indexOf(letter) === -1) {
+        // Make sure we didn't use this letter
+        if (userGuesses.indexOf(letter) === -1) {
             userGuesses.push(letter);
-            evaluateguess(letter);
+            evaluateGuess(letter);
         }
     }
 };
 
-//Event listener
+// Event listener
 document.onkeydown = function(event) {
-    //restart game when done
+    //if the game is finished, restart it.
     if(finishedGame) {
         startGame();
         finishedGame = false;
+
     } else {
-        //check if letter was pressed
+        // Check to make sure a-z was pressed.
         if(event.keyCode >= 65 && event.keyCode <= 90) {
-            keypress.play()
+            key.play();
             makeGuess(event.key.toUpperCase());
-            refreshscreen();
-            didyouwin();
-            didyoulose();
+            refreshScreen();
+            checkWin();
+            checkLoss();
         }
     }
 };
-    
